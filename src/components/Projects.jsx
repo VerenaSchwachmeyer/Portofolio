d4ee1fadcc17aa27ce7f1571994883158a4555af
VerenaSchwachmeyer <Vronus@gmx.de>
2024-07-30 09:44:04 +0200

    updated packages

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,6 +7,10 @@ import Flowerbauer1 from "./Projects/Flowerbauer/Flowerbauer1";
 import Flowerbauer2 from "./Projects/Flowerbauer/Flowerbauer2";
 import Flowerbauer3 from "./Projects/Flowerbauer/Flowerbauer3";
 import Flowerbauer4 from "./Projects/Flowerbauer/Flowerbauer4";
+import Downtown1 from "./Projects/Downtown/Downtown1";
+import Downtown2 from "./Projects/Downtown/Downtown2";
+import Downtown3 from "./Projects/Downtown/Downtown3";
+import Downtown4 from "./Projects/Downtown/Downtown4";
 import Linov1 from "./Projects/Linov/Linov1";
 import { useTranslation } from "react-i18next";
 
@@ -21,7 +25,7 @@ function Projects() {
     <Flowerbauer4 />,
   ];
 
-  // const itemsDT = [<Downtown1 />, <Downtown2 />, <Downtown3 />, <Downtown4 />];
+  const itemsDT = [<Downtown1 />, <Downtown2 />, <Downtown3 />, <Downtown4 />];
 
   const responsive = {
     0: { items: 1 },
@@ -33,7 +37,7 @@ function Projects() {
       <div className="content">
         <h2 className="sectionTitle">{t("projects.heading")}</h2>
         <p className="aboutmeText">{t("projects.text")}</p>
-        {/* <AliceCarousel
+        <AliceCarousel
           role="list"
           aria-label="Project named Downtown"
           mouseTracking
@@ -62,7 +66,7 @@ function Projects() {
               />
             );
           }}
-        /> */}
+        />
         <AliceCarousel
           role="list"
           aria-label="Project named Downtown"
